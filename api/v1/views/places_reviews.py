dd64d5b55b09238938a5f0dcb8b2063134abfc5c
MundiaNderi <sarahngima77@gmail.com>
2023-05-29 16:20:44 +0300

    Places Reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python3
 
 '''
-Create a new view for City objects that handles
+Create a new view for Review objects that handles
 all default RestFul API actions.
 '''
 
 from api.v1.views import app_views
 from flask import jsonify, request, abort
-from models import storage
 from api.v1.views import get, delete, post, put
 
 
-@app_views.route('/cities/<city_id>',
+@app_views.route('/reviews/<review_id>',
                  strict_slashes=False,
                  methods=['GET', 'DELETE', 'PUT'])
-@app_views.route('/states/<state_id>/cities',
+@app_views.route('/places/<place_id>/reviews',
                  strict_slashes=False,
                  methods=['GET', 'POST'])
-def city_crud(state_id=None, city_id=None):
+def review_crud(place_id=None, review_id=None):
     '''Returns GET, DELETE, PUT, POST methods'''
     data = {
-        'str': 'City',
-        '_id': city_id,
-        'p_id': state_id,
-        'p_prop': 'state_id',
-        'p_child': 'cities',
-        'p_str': 'State',
-        'check': ['name'],
-        'ignore': ['created_at', 'updated_at', 'id', 'state_id']}
+        'str': 'Review',
+        '_id': review_id,
+        'p_id': place_id,
+        'p_prop': 'place_id',
+        'p_child': 'reviews',
+        'p_str': 'Place',
+        'check': ['user_id', 'text'],
+        'ignore': ['id',
+                   'user_id',
+                   'place_id',
+                   'created_at',
+                   'updated_at']}
     methods = {
         'GET': get,
         'DELETE': delete,
