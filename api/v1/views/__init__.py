9034011e54ddb92f1fb4836c789496d58c15cb37
MundiaNderi <sarahngima77@gmail.com>
2023-05-29 16:44:34 +0300

    CORS

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,15 +3,14 @@
 This script initializes the blueprint for API views.
 """
 
-
-from api.v1.views.places_reviews import *
-from api.v1.views.places import *
-from api.v1.views.users import *
-from api.v1.views.amenities import *
-from api.v1.views.cities import *
-from api.v1.views.states import *
-from api.v1.views.places_amenities import *
 from api.v1.views.index import *
+from api.v1.views.places_amenities import *
+from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
 from flask import Blueprint
 from models.state import State
 from models.amenity import Amenity
