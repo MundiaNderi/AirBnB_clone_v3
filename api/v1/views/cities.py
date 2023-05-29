0f1d2cd1311ff08a06d5e78cccad33c1a2a3538b
MundiaNderi <sarahngima77@gmail.com>
2023-05-29 16:16:02 +0300

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -4,49 +4,35 @@
 Create a new view for City objects that handles
 all default RestFul API actions.
 '''
-from flask import Flask, jsonify, request, abort
+
+from api.v1.views import app_views
+from flask import jsonify, request, abort
 from models import storage
-from models.city import City
-from models.state import State
-from flask import Blueprint
-
-cities = Blueprint('cities', __name__, url_prefix='/cities')
+from api.v1.views import get, delete, post, put
 
 
-@cities.route('/', methods=['GET'])
-def get_cities():
-    cities = storage.all(City).values()
-    cities_dict = [city.to_dict() for city in cities]
-    return jsonify(cities_dict)
-
-
-@cities.route('/states/<state_id>/cities', methods=['POST'])
-def create_city(state_id):
-    state = storage.get(State, state_id)
-    if not state:
-        abort(404)
-
-    if not request.is_json:
-        abort(400, description='Not a JSON')
-
-    data = request.get_json()
-    if 'name' not in data:
-        abort(400, description='Missing name')
-
-    city = City()
-    city.name = data['name']
-    city.state_id = state_id
-    storage.new(city)
-    storage.save()
-
-    return jsonify(city.to_dict()), 201
-
-
-@cities.route('/<city_id>', methods=['DELETE'])
-def delete_city(city_id):
-    city = storage.get(City, city_id)
-    if not city:
-        abort(404)
-    storage.delete(city)
-    storage.save()
-    return jsonify({})
+@app_views.route('/cities/<city_id>',
+                 strict_slashes=False,
+                 methods=['GET', 'DELETE', 'PUT'])
+@app_views.route('/states/<state_id>/cities',
+                 strict_slashes=False,
+                 methods=['GET', 'POST'])
+def city_crud(state_id=None, city_id=None):
+    '''Returns GET, DELETE, PUT, POST methods'''
+    data = {
+        'str': 'City',
+        '_id': city_id,
+        'p_id': state_id,
+        'p_prop': 'state_id',
+        'p_child': 'cities',
+        'p_str': 'State',
+        'check': ['name'],
+        'ignore': ['created_at', 'updated_at', 'id', 'state_id']}
+    methods = {
+        'GET': get,
+        'DELETE': delete,
+        'POST': post,
+        'PUT': put
+    }
+    if request.method in methods:
+        return methods[request.method](data)
