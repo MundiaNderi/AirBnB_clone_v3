829bd2693759bbe20d5a1c7fe14d1c74e503b34e
Lynne Michuki <lynnenmichuki@gmail.com>
2023-05-30 10:55:53 +0300

    Update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,11 @@
 This script initializes the blueprint for API views.
 """
 
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+<<<<<<< HEAD
+
 from api.v1.views.places_amenities import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places import *
@@ -10,12 +15,6 @@ from api.v1.views.users import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
-from api.v1.views.index import *
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-<<<<<<< HEAD
-
 from api.v1.views.index import *
 
 =======
