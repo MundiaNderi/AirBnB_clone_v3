1008d6a5d983e79322423eec23d39b66d2c0094a
MundiaNderi <sarahngima77@gmail.com>
2023-05-30 09:45:44 +0300

    Documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,29 +8,6 @@ JSON response containing the counts of different classes (amenities,
 cities, places, reviews, states, and users) in the storage.
 
 Usage: This script should be imported and used within a Flask application.
-
-Example:
-    # Assuming this script is named 'stats.py' and resides in the
-    'api.v1.views' module from api.v1.views.stats import get_stats
-
-    app_views.add_url_rule('/stats', 'get_stats', get_stats, methods=['GET'])
-
-    This code snippet adds the '/stats' route to the Flask app with
-    the 'get_stats' function as the handler.
-
-API Endpoint:
-    - GET /stats: Returns statistics about different classes
-    as a JSON response.
-
-        Response example:
-        {
-            "amenities": 10,
-            "cities": 20,
-            "places": 50,
-            "reviews": 30,
-            "states": 5,
-            "users": 100
-        }
 """
 
 from api.v1.views import app_views
@@ -42,11 +19,9 @@ from models import storage
 def get_stats():
     """
     Retrieve statistics about different classes.
-
     Returns:
         Response: A Flask JSON response containing the counts of various
         classes in the storage.
-
     Raises:
         None
     """
