76247764f7fedb8737533b805ed5c4ea5cd28240
MundiaNderi <sarahngima77@gmail.com>
2023-05-30 10:19:08 +0300

    Fixing pycodestyle for states file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,10 +8,12 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
+
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
@@ -20,7 +22,9 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def del_state(state_id):
     state = storage.get(State, state_id)
     if not state:
@@ -28,6 +32,7 @@ def del_state(state_id):
     storage.delete(state)
     storage.save()
     return jsonify({}), 200
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -39,6 +44,7 @@ def create_state():
     state = State(**data)
     state.save()
     return jsonify(state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
