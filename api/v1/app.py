f0c6ef4b1588ab8c6e2dab2926386cdd1bc541c3
MundiaNderi <sarahngima77@gmail.com>
2023-05-30 09:26:41 +0300

    Task 3 Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,62 @@
 #!/usr/bin/python3
 """
-Script that creates a file with a Flask instance app
+This script creates a Flask application instance with a blueprint
+for API endpoints.
+
+It initializes the Flask app, sets up CORS (Cross-Origin Resource Sharing),
+and registers the blueprint for the API endpoints. It also defines error
+handlers and a teardown function to close the database connection after
+each request.
+
+Usage: python3 <script_name>.py
+
+Environment variables:
+    - HBNB_API_HOST: The host IP to bind the Flask server (default: '0.0.0.0')
+    - HBNB_API_PORT: The port to run the Flask server (default: 5000)
 """
+
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 import os
 from os import getenv
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views, url_prefix='/api/v1')
+app.url_map.strict_slashes = False
+cors = CORS(app, resources={"*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """
-    Closes the database connection at the end of the request
+    Closes the database connection at the end of the request.
+
+    Args:
+        exception (Exception): The exception object if any
+        occurred during the request.
     """
     storage.close()
 
 
-# Handler for 404 errors
 @app.errorhandler(404)
 def handle_404_error(error):
     """
-    Handler for 404 errors
+    Handles 404 errors and returns a JSON response.
+
+    Args:
+        error (Exception): The exception object representing the 404 error.
+
+    Returns:
+        Response: A Flask JSON response indicating that the requested
+        resource was not found.
     """
     response = jsonify({'error': 'Not found'})
     response.status_code = 404
     return response
 
 
-# Run the Flask server
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST')
     port = int(os.getenv('HBNB_API_PORT'))
@@ -40,4 +64,4 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if port is None:
         port = 5000
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=host, port=port)
